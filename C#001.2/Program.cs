56b00bebf1c51293dbd49d590cfea2a3f20ed8b6
Алексей <110294020+Alexeii-Toropov@users.noreply.github.com>
2022-08-07 22:33:55 +0300

    Add files via upload

---
+++ C#001.2/Program.cs
@@ -0,0 +1,48 @@
+﻿Console.Write("Введите 1 число  ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2 число  ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2 число  ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+{
+    int max = number1;
+    if (max > number3)
+    {
+        Console.Write("max = ");
+        Console.Write(max);
+    }
+    else
+    {
+        max = number3;
+        Console.Write("max = ");
+        Console.Write(max);
+    }
+}
+else if (number1 > number3)
+{
+    int max = number1;
+    if (max > number2)
+    {
+        Console.Write("max = ");
+        Console.Write(max);
+    }
+    else
+    {
+        max = number2;
+        Console.Write("max = ");
+        Console.Write(max);
+    }
+}
+else if (number2 > number3)
+{
+    int max = number2;
+    Console.Write("max = ");
+    Console.Write(max);
+}
+else
+{   
+    Console.Write("max = ");
+    Console.Write(number3);
+}
