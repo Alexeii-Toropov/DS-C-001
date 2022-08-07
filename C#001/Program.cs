86c9d88f9e4833a0dc2c9992a0de936a8a5509cd
Алексей <110294020+Alexeii-Toropov@users.noreply.github.com>
2022-08-07 22:46:42 +0300

    Add files via upload

---
+++ C#001/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите 1 число  ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2 число  ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.Write("max = ");
+    int max = number1;
+    Console.Write(max);
+}
+else
+{
+    Console.Write("max = ");
+    int max = number2;
+    Console.Write(max);
+}
