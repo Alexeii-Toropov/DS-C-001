45107b57e1b057f403ee9b9813f594cae63dd059
Алексей <110294020+Alexeii-Toropov@users.noreply.github.com>
2022-08-08 11:44:42 +0300

    Add files via upload

---
+++ C#001.4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число  ");
+int number = Convert.ToInt32(Console.ReadLine());
+int res = 2;
+int mn = number / 2;
+
+while (mn > 0)
+{
+    res = res * mn;
+    Console.Write(res);
+    Console.Write(", ");
+    res = 2;
+    mn--;
+}
